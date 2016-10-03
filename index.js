4c2fb9a75baadfb59732ad412ea1d890cb0111d1
albert <albert.fang@tarokosoftware.com>
2016-10-03 17:59:30 +0800

    c

--- index.js
+++ index.js
@@ -8,7 +8,7 @@
 const firstMod1 = require('./first-module');
 const firstMod2 = require('./first-module');
 
-firstMod1.put('put value');
+firstMod1.put('put value!');
 console.log(firstMod2.get());
 
 // const configurableMod = require('./configurable-module');
